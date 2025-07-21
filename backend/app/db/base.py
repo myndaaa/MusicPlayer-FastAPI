2b7253ad909f994388fed6f9f6b296dbefb39112
myndaaa <myshaaa31ns@gmail.com>
2025-07-21 17:19:51 +0600

    updated and implemented database session and configuration files of the environment

---
+++ backend/app/db/base.py
@@ -0,0 +1,24 @@
+
+from app.db.base_class import Base  
+
+from app.db.models.album_songs import AlbumSongs #1
+from app.db.models.album import Album #2
+from app.db.models.artist_band_members import ArtistBandMember #3
+from app.db.models.artist import Artist  #4
+from app.db.models.audit_log import AuditLog  #5
+from app.db.models.band import Band  #6
+from app.db.models.following import Following #7
+from app.db.models.genre import Genre #8
+from app.db.models.history import History #9
+from app.db.models.likes import Likes #10
+from app.db.models.localization import Localization #11
+from app.db.models.payment import Payment #12
+from app.db.models.playlist_collaborator import PlaylistCollaborator #13
+from app.db.models.playlist_songs import PlaylistSongs #14
+from app.db.models.playlist import Playlist #15
+from app.db.models.song import Song #16
+from app.db.models.subscription_plan import SubscriptionPlan #17
+from app.db.models.system_config import SystemConfig #18
+from app.db.models.user_subscription import UserSubscription #19
+from app.db.models.user import User #20
+
