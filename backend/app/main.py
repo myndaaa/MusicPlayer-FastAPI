0bdf53e7dfca24b1c67ea05f1c3f5cb3497e1006
myndaaa <myshaaa31ns@gmail.com>
2025-07-22 12:58:00 +0600

    fix: updated main to remove test console logs

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,13 +7,16 @@ app = FastAPI(
     description="Backend API for the Music Player App",
     version="0.1.0"
 )
+
 # Routers Imports
-
-
+'''
+from app.api.v1 import user_router, song_router, etc...
+'''
 # Router inclusion and prefix declaration
-
-
-
+'''
+app.include_router(user.router, prefix="/api/v1/users")
+app.include_router(song.router, prefix="/api/v1/songs")
+'''
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
@@ -22,15 +25,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# dev health check route
-@app.get("/")
-def read_root():
-    return {"status": "API is live 🚀"}
-
-# Test
-
-from app.core.config import settings
-
-print(settings.DATABASE_URL)
-print(settings.JWT_SECRET_KEY)
